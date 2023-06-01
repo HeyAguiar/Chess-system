bca07d6e95648ae1d6441538e31c6307b05bd80f
Guilherme Aguiar <mechamedetaki@gmail.com>
2023-05-31 21:57:29 -0300

    First class: Position

--- src/main/java/chess/system/ChessSystem.java
+++ src/main/java/chess/system/ChessSystem.java
@@ -1,8 +1,11 @@
 package chess.system;
 
+import boardgame.Position;
+
 public class ChessSystem {
 
     public static void main(String[] args) {
-       
+       Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
