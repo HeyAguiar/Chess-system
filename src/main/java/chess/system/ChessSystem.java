bd66934a30a0c2f0fff212419dab63a0703bebf0
Guilherme Aguiar <mechamedetaki@gmail.com>
2023-06-04 17:35:40 -0300

    Printing possible moves

--- src/main/java/chess/system/ChessSystem.java
+++ src/main/java/chess/system/ChessSystem.java
@@ -21,7 +21,10 @@ public class ChessSystem {
              System.out.println();
              System.out.println("Source: ");
              ChessPosition source = UI.readChessPosition(sc);
-
+             
+             boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+             UI.clearScreen();
+             UI.printBoard(chessMatch.getPieces(), possibleMoves);
              System.out.println();
              System.out.println("Target: ");
              ChessPosition target = UI.readChessPosition(sc);
@@ -36,4 +39,5 @@ public class ChessSystem {
               sc.nextLine();
       }
     }
+  }
 }
