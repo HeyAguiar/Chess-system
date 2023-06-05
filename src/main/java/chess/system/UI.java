75a69b9bbb335e6e0e09522df8d9903d985ef38f
Guilherme Aguiar <mechamedetaki@gmail.com>
2023-06-04 22:21:19 -0300

    Check logic

--- src/main/java/chess/system/UI.java
+++ src/main/java/chess/system/UI.java
@@ -52,6 +52,10 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turn : " + chessMatch.getTurn());
+        System.out.println("Waiting player " + chessMatch.getCurrentPlayer());
+        if (chessMatch.getCheck()) {
+            System.out.println("CHECK!");
+        }
     }
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i=0; i<pieces.length; i++) {
